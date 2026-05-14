08c101eee7396ddeb09b873e4b8855a7958c7c45
Gubi Fortnite Yallah Habibi Jesko <jesko.kissner@mailbox.org>
2026-05-14 16:46:10 +0200

    folder and first startscreen idea

---
+++ megastar.Game/screens/MainScreen.cs
@@ -0,0 +1,68 @@
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Screens;
+using osuTK;
+using osuTK.Graphics;
+
+namespace megastar.Game
+{
+    public partial class MainScreen : Screen
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            InternalChildren = new Drawable[]
+            {
+                //Background
+                new Box
+                {
+                    Colour = Color4.Violet,
+                    RelativeSizeAxes = Axes.Both,
+                },
+                //Title
+                new SpriteText
+                {
+                    Y = 20,
+                    Text = "ULTRA MEGA PREMIUM DELUXE KARAOKE",
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.TopCentre,
+                    Font = FontUsage.Default.With(size: 80),
+                    Colour = Color4.Purple,
+                    Shadow = true,
+                    ShadowColour = Color4.Pink,
+                },
+                new FillFlowContainer()
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    AutoSizeAxes = Axes.Both, // Shrink-wraps to fit the buttons
+                    Direction = FillDirection.Vertical,
+                    Spacing = new Vector2(0, 10), // Gap between buttons
+                    Children = new Drawable[]
+                    {
+                        new BasicButton
+                        {
+                            Text = "Start Game",
+                            Size = new Vector2(200, 40),
+                            BackgroundColour = Color4.White,
+                        },
+                        new BasicButton
+                        {
+                            Text = "Search",
+                            Size = new Vector2(200, 40),
+                        }
+                    }
+                }
+                /*
+                new SpinningBox
+                {
+                    Anchor = Anchor.Centre,
+                } */
+            };
+        }
+    }
+}
