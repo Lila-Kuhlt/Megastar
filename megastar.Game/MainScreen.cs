ce442de929ecfe72d05122dd28109bbb41501db9
Gubi Fortnite Yallah Habibi Jesko <jesko.kissner@mailbox.org>
2026-05-14 16:27:01 +0200

    first commit

--- megastar.Game/MainScreen.cs
+++ megastar.Game/MainScreen.cs
@@ -2,7 +2,9 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Screens;
+using osuTK;
 using osuTK.Graphics;
 
 namespace megastar.Game
@@ -22,15 +24,26 @@ namespace megastar.Game
                 new SpriteText
                 {
                     Y = 20,
-                    Text = "Main Screen",
+                    Text = "ULTRA MEGA PREMIUM DELUXE KARAOKE",
                     Anchor = Anchor.TopCentre,
                     Origin = Anchor.TopCentre,
-                    Font = FontUsage.Default.With(size: 40)
+                    Font = FontUsage.Default.With(size: 80),
+                    Colour = Color4.Purple,
+                    Shadow = true,
+                    ShadowColour = Color4.Pink,
                 },
+                new BasicButton()
+                {
+                    Text = "Play",
+                    Size = new Vector2(200, 200),
+                    Anchor = Anchor.Centre,
+                    BackgroundColour = Color4.Orange,
+                },
+                /*
                 new SpinningBox
                 {
                     Anchor = Anchor.Centre,
-                }
+                } */
             };
         }
     }
