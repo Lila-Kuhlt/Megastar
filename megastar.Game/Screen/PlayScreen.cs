b8e1697b8fb0bae8a6544fbd3daec39219082861
Gubi Fortnite Yallah Habibi Jesko <jesko.kissner@mailbox.org>
2026-05-15 16:53:16 +0200

    Loooooool

--- megastar.Game/Screen/PlayScreen.cs
+++ megastar.Game/Screen/PlayScreen.cs
@@ -1,8 +1,12 @@
 using megastar.Game.presets;
 using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Audio.Track;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
 using osu.Framework.Screens;
 using osuTK.Graphics;
 
@@ -10,9 +14,13 @@ namespace megastar.Game.screens;
 
 public partial class PlayScreen : Screen
 {
+    private Track track;
+
     [BackgroundDependencyLoader]
-    private void load()
+    private void load(AudioManager audio)
     {
+        track = loadSong(audio, @"C:\Users\jesko\RiderProjects\Megastar\megastar.Resources\Tracks", "Abba - Thank You For The Music.mp3");
+
         InternalChildren = new Drawable[]
         {
             //Background
@@ -30,5 +38,29 @@ public partial class PlayScreen : Screen
             },
             new BackButton(this.Exit, "Go Back")
         };
+    }
+
+    public override void OnEntering(ScreenTransitionEvent e)
+    {
+        base.OnEntering(e);
+        track?.Start();
+    }
+
+    //stop the track when leaving the screen so it doesn't leak into the menu
+    public override bool OnExiting(ScreenExitEvent e)
+    {
+        track?.Stop();
+        track?.Dispose();
+
+        return base.OnExiting(e);
+    }
+
+    //FIXME hier könnte sehr viel overhead entstehen wenn das jedes mal neu geladen wird aber Turingmachine hat unendlich speicher
+    private Track loadSong(AudioManager audioManager, string directoryPath, string fileName)
+    {
+        var storage = new NativeStorage(directoryPath);
+        var resourceStore = new StorageBackedResourceStore(storage);
+        ITrackStore customTrackStore = audioManager.GetTrackStore(resourceStore);
+        return customTrackStore.Get(fileName);
     }
 }
