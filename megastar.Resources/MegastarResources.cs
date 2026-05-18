139e57b3997e20cfa2c1e36dcd94011220e18839
Aaron Geiger <aaron@geigr.dev>
2026-05-18 20:25:29 +0200

    Use ResourceAssembly for embedded resources

--- megastar.Resources/MegastarResources.cs
+++ megastar.Resources/MegastarResources.cs
@@ -1,9 +1,31 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace megastar.Resources
 {
     public static class MegastarResources
     {
+        private static string resourcePrefix = "megastar.Resources";
         public static Assembly ResourceAssembly => typeof(MegastarResources).Assembly;
+
+        /// <summary>
+        /// Retrieves a resource from the ResourceAssembly.
+        ///
+        /// Note: The requested resource has to be included in the assembly. For this see project file.
+        /// </summary>
+        public static string ReadResouceFile(string fileName)
+        {
+            string resourceName = ResourceAssembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(fileName));
+
+            using Stream stream = ResourceAssembly.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new StreamReader(stream ??
+                                                         throw new FileNotFoundException(
+                                                             $"Could not load {resourceName} from ResourceAssembly. Does the File exist?"));
+
+            return reader.ReadToEnd();
+        }
     }
 }
