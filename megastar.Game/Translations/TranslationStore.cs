1d8b387b831c155d47c9a90927bd82f7fc4708ed
uxepd <uxepd@student.kit.edu>
2026-05-22 17:48:50 +0200

    [feat] add passthrough methods to fluent to avoid method chaining

--- megastar.Game/Translations/TranslationStore.cs
+++ megastar.Game/Translations/TranslationStore.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Linguini.Bundle.Builder;
 using Linguini.Bundle;
+using Linguini.Shared.Types.Bundle;
 using megastar.Resources;
 using osu.Framework.IO.Stores;
 
@@ -40,6 +41,30 @@ public class TranslationStore : IResourceStore<FluentBundle>
     public void SetDefaultLanguage(string language)
     {
         selectedLanguage = language;
+    }
+
+    /// <summary>
+    ///     Passthrough method for <see cref="IReadBundle.GetAttrMessage" />, so there is less method chaining.
+    /// </summary>
+    /// <param name="language">The language to use.</param>
+    /// <param name="msgWithAttr">The message with attribute to retrieve.</param>
+    /// <param name="args">Optional arguments to format the message.</param>
+    /// <returns>The attribute message from the read bundle.</returns>
+    public string GetAttrMessage(string msgWithAttr, params (string, IFluentType)[] args)
+    {
+        return GetAttrMessage(selectedLanguage, msgWithAttr, args);
+    }
+
+    /// <summary>
+    ///     Passthrough method for <see cref="IReadBundle.GetAttrMessage" />, so there is less method chaining. If possible use <see cref="GetAttrMessage(string, ValueTuple{string, IFluentType}[])"/>
+    /// </summary>
+    /// <param name="language">The language to use.</param>
+    /// <param name="msgWithAttr">The message with attribute to retrieve.</param>
+    /// <param name="args">Optional arguments to format the message.</param>
+    /// <returns>The attribute message from the read bundle.</returns>
+    public string GetAttrMessage(string language, string msgWithAttr, params (string, IFluentType)[] args)
+    {
+        return Get(language).GetAttrMessage(msgWithAttr, args);
     }
 
     /// <summary>
